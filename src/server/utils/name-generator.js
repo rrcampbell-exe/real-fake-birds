113187a7b6591fdb3f0524245604dd014ba3cef7
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-04 17:51:38 -0500

    feat: introduce colors api

--- src/server/utils/name-generator.js
+++ src/server/utils/name-generator.js
@@ -1,19 +1,21 @@
 const locations = require('../constants/locations')
-const colors = require('../constants/colors')
 const bodyParts = require('../constants/body-parts')
 const birdTypes = require ('../constants/bird-types')
+
+const colorSelect = require('../utils/color-select')
 
 // TODO: introduce adjectives to flow
 const generalAdjectives = require('../constants/general-adjectives')
 const bodyPartAdjectives = require('../constants/body-part-adjectives')
 
-const nameGenerator = () => {
+
+const nameGenerator = async () => {
   const locationVal = (Math.floor(Math.random() * locations.length)) // TODO: set this to only display some percentage of the time
-  const colorVal = Math.floor(Math.random() * colors.length) // TODO: set this to only display a percentage of the time
+  const color = await colorSelect() // TODO: set this to only display some percentage of the time
   const bodyPartVal = Math.floor(Math.random() * bodyParts.length) // TODO: set this to display only if color is being displayed, and sometimes, not display even then
   const birdTypeVal = Math.floor(Math.random() * birdTypes.length)
 
-  return `${locations[locationVal]} ${colors[colorVal]}${bodyParts[bodyPartVal]} ${birdTypes[birdTypeVal]}`
+  return `${locations[locationVal]} ${color}${bodyParts[bodyPartVal]} ${birdTypes[birdTypeVal]}`
 }
 
 module.exports = nameGenerator
