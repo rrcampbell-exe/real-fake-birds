83d7adbc545f85c61123702c7a382677c89b5762
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-04 13:18:25 -0500

    feat: introduce constants

---
+++ src/server/constants/bird-types.js
@@ -0,0 +1,19 @@
+const birdTypes = [
+  'Robin',
+  'Swallow',
+  'Duck',
+  'Owl',
+  'Sparrow',
+  'Hummingbird',
+  'Woodpecker',
+  'Wren',
+  'Finch',
+  'Swan',
+  'Goose',
+  'Peafowl',
+  'Nighthawk',
+  'Parrot',
+  'Toucan'
+]
+
+module.exports = birdTypes
---
+++ src/server/constants/body-parts.js
@@ -0,0 +1,12 @@
+const bodyParts = [
+  '-bellied',
+  '-eyed',
+  '-tailed',
+  '-breasted',
+  '-beaked',
+  '-horned',
+  '-winged',
+  '-vented'
+]
+
+module.exports = bodyParts
