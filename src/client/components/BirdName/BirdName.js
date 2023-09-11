0daa162a3b6a68bd51b921b06c1ee166a0bd5ec5
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-11 06:25:56 -0500

    feat: institute modal

--- src/client/components/BirdName/BirdName.js
+++ src/client/components/BirdName/BirdName.js
@@ -2,24 +2,12 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import AnswerModal from '../Modal'
 
-const responseEval = (isReal, chosenResponse) => {
-  // TODO: replace alerts with modal
-  if (isReal && chosenResponse === 'real') {
-    alert('Correct!')
-    return
-  }
-  if (!isReal && chosenResponse === 'fake') {
-    alert('Correct!')
-    return
-  }
-  alert('Wrong!')
-}
-
 const BirdName = () => {
   const [birdData, setBirdData] = useState('')
   const [isReal, setIsReal] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isCorrect, setIsCorrect] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,15 +20,33 @@ const BirdName = () => {
     fetchData()
   }, [])
 
+  const responseEval = (e, isReal, chosenResponse) => {
+    // TODO: refactor for simplicity, as both if statements run same functions
+    e.preventDefault()
+    if (isReal && chosenResponse === 'real') {
+      setIsModalOpen(true)
+      setIsCorrect(true)
+      return
+    }
+    if (!isReal && chosenResponse === 'fake') {
+      setIsModalOpen(true)
+      setIsCorrect(true)
+      return
+    }
+    setIsModalOpen(true)
+  }
+
   if (isLoading) return 'loading'
   return (
   <>
     <h1>{birdData}</h1>
-    <button id='real' onClick={(e) => responseEval(isReal, e.target.id)}>Real Bird</button>
-    <button id='fake' onClick={(e) => responseEval(isReal, e.target.id)}>Fake Bird</button>
-    <button onClick={() => setIsModalOpen(true)}>Try Again</button>
+    <button id='real' onClick={(e) => responseEval(e, isReal, e.target.id)}>Real Bird</button>
+    <button id='fake' onClick={(e) => responseEval(e, isReal, e.target.id)}>Fake Bird</button>
     <AnswerModal
       isOpen={isModalOpen}
+      birdData={birdData}
+      isReal={isReal}
+      isCorrect={isCorrect}
     />
   </>
   )
--- src/client/components/Modal.js
+++ src/client/components/Modal.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import ReactModal from 'react-modal'
+import styled from 'styled-components'
 
-const AnswerModal = ({ birdData, isReal }) => {
+const Modal = styled.div`
+  display: ${props => props.isOpen ? 'flex' : 'none'};
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  right: auto;
+  bottom: auto;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  padding: 10rem;
+  border: solid black;
+  border-radius: 0.5rem; 
+  background-color: #1DA1F2;
+  color: white;
+`
+
+const AnswerModal = ({ isOpen, birdData, isReal, isCorrect }) => {
   return (
-    <ReactModal>
-      <h1>oof ouch my bones</h1>
-    </ReactModal>
+    <Modal
+      isOpen={isOpen}
+    >
+      <h1>{isCorrect ? 'Correct!' : 'Incorrect!'}</h1>
+      <p>The {birdData} is a {isReal ? 'real' : 'fake'} bird.</p>
+      <button onClick={() => window.location.reload(true)}>Try Again</button>
+    </Modal>
   )
 }
 
