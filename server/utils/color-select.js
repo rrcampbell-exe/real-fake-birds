f054f2aea4fab04057b140bd09343a1f5d24a4e3
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-15 14:52:07 -0500

    fix: remove TODOs

--- server/utils/color-select.js
+++ server/utils/color-select.js
@@ -4,7 +4,6 @@ const colorSelect = async () => {
   const colorVal = Math.floor(Math.random() * 1000000)
   const response = await axios.get(`https://www.thecolorapi.com/id?hex=${colorVal}`)
   
-  // TODO: filter out any colors with spaces in them, run again
   return response.data.name.value
 }
 
