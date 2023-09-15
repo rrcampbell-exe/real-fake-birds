4f14b4a4ed5d59e82ed139146b1d85111e999fbf
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-15 14:41:44 -0500

    feat: introduce footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BirdName from './components/BirdName'
 import PageTitle from './components/PageHeader';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <PageTitle />
       <BirdName />
+      <Footer />
     </>
   );
 }
