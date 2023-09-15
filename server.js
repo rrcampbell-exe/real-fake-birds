88cc1985fada590dae7d8c1528feeee679026a66
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-15 07:59:28 -0500

    fix: simplify server

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors')
 
 const routes = require('./src/server')
-const path = require('path')
 const app = express();
 const port = process.env.PORT || 3001
 
@@ -10,12 +9,6 @@ app.use(cors())
 
 // create a GET route
 app.use(routes)
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Chirp-chirp! The server is now listening on port ${port}!`))
