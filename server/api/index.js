1b69cacd275a85f1cb2745375399d01f21289f1c
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-17 13:58:01 -0500

    fix: use dynamic geography in algorithm

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const API_KEY = process.env.REACT_APP_API_KEY
-const nameGenerator = require ('../utils/name-generator')
+const nameGenerator = require('../utils/name-generator')
+const locations = require('../constants/locations')
 
-const url = "https://api.ebird.org/v2/data/obs/US/recent"
+const selectLocation = () => {
+  const locationVal = (Math.floor(Math.random() * locations.length))
+  const { alpha2code } = locations[locationVal]
+  return alpha2code
+}
+
 const options = {
   headers: {
     'X-eBirdApiToken': API_KEY
@@ -21,12 +27,15 @@ const birdCall = async (url, options) => {
       return response.json()
     })
     .catch(error => console.log('error', error));
-  const random = Math.floor(Math.random() * response.length)
-  return { birdName: response[random].comName, isReal: true }
+  if (response.length) {
+    const random = Math.floor(Math.random() * response.length)
+    return { birdName: response[random].comName, isReal: true, url }
+  }
+  return nameGenerator()
 }
 
 router.get('/', async (req, res) => {
-  const birdData = await birdCall(url, options)
+  const birdData = await birdCall(`https://api.ebird.org/v2/data/obs/${selectLocation()}/recent`, options)
   res.send(birdData)
 })
 
