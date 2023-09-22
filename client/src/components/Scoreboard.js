5b2f507d555a3ec312564ff59e3cf3ff2a48da03
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-22 09:58:08 -0500

    fix: import goal utils

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import ProgressBar from './ProgressBar'
 import streakValues from '../constants/streak-values'
 import milestoneValues from '../constants/milestone-values'
+import { isNextStreakEval, isNextMilestoneEval, currentStreakGoalEval, currentMilestoneGoalEval } from '../utils/goals-evaluation'
 
 const ScoreboardContainer = styled.div`
   display: flex;
@@ -57,27 +58,18 @@ const MobileFlexContainer = styled.div`
   }
 `
 
-// TODO: refactor streaks/milestones functionality to minimize duplicate code where possible
-// TODO: make reset button available only during development (or hide it in options menu, eventually)
-
 const Scoreboard = () => {
   const birdScore = JSON.parse(localStorage.getItem('birdScore'))
   const { birdsSeen, birdsIdentified, currentStreak } = birdScore
 
-  const areAvailableStreaks = (streakValues) => streakValues > currentStreak
-  const isNextStreak = streakValues.filter(areAvailableStreaks)[0]
+  const isNextStreak = isNextStreakEval(streakValues, currentStreak)
+  const isCurrentStreakGoal = currentStreakGoalEval(streakValues, currentStreak)
 
-  const areAvailableMilestones = (milestoneValues) => milestoneValues > birdsIdentified
-  const isNextMilestone = milestoneValues.filter(areAvailableMilestones)[0]
-
-  const currentStreakGoalEval = (streakValues) => streakValues === currentStreak
-  const isCurrentStreakGoal = streakValues.filter(currentStreakGoalEval)[0]
-
-  const currentMilestoneGoalEval = (milestoneValues) => milestoneValues === birdsIdentified
-  const isCurrentMilestoneGoal = milestoneValues.filter(currentMilestoneGoalEval)[0]
-      
-  const justAchievedStreak = localStorage.getItem('birdStreakAchieved') === 'true' ? true : false
-  const justAchievedMilestone = localStorage.getItem('birdMilestoneAchieved') === 'true' ? true : false
+  const isNextMilestone = isNextMilestoneEval(milestoneValues, birdsIdentified)
+  const isCurrentMilestoneGoal = currentMilestoneGoalEval(milestoneValues, birdsIdentified)
+  
+  const justAchievedStreak = JSON.parse(localStorage.getItem('birdStreakAchieved'))
+  const justAchievedMilestone = JSON.parse(localStorage.getItem('birdMilestoneAchieved'))
 
   const maxValue = (currentValue, isNextGoal, isCurrentGoal, justAchievedGoal) => {
     if ((currentValue === isCurrentGoal) && justAchievedGoal) {
