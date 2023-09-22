5109e3bb229b81e341d494444a03247faea79262
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-22 09:23:18 -0500

    fix: update styles

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -5,22 +5,19 @@ import streakValues from '../constants/streak-values'
 import milestoneValues from '../constants/milestone-values'
 
 const ScoreboardContainer = styled.div`
-  position: fixed;
-  top: 75%;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  margin: 0 1rem 0 1rem;
   color: white;
   font-size: 0.75rem;
-  width: 33%;
+  background-color: rgba(0,0,0,.35);
   padding: 0.5rem;
+  border: solid white 1px;
+  border-radius: 4px;
   h4 {
-    margin-top: 0rem;
-    margin-bottom: 0rem;
+    margin-top: 0.25rem;
+    margin-bottom: 0.5rem;
   }
-  h4:first-child {
-      margin-top: 1rem;
-      margin-bottom: 0rem;
-    }
   a {
     margin-top: 0.25rem;
     color: white;
@@ -31,53 +28,35 @@ const ScoreboardContainer = styled.div`
   }
 
   @media (min-width: 600px) {
-    top: 80%;
-    flex-direction: row;
+    position: fixed;
+    top: 0;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    margin: 1.5rem 1rem 0 0;
     padding: 0;
     border: none;
-    width: 100%;
     background-color: rgba(0,0,0,0);
-    h4:first-child {
-      margin-top: 0;
-    }
-    #answers-container {
-      align-items: flex-end;
+    #streak-progress-container {
+      width: 100%;
     }
   }
 `
 
-const StreakContainer = styled.div`
-  width: 100%;
+const MobileFlexContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: start;
-  padding: 0 1rem;
-  h2 {
-      margin: 0.5rem 0 0.2rem 0;
-    }
-  #answers-label {
-      margin-bottom: 0.65rem;
-    }
-
+  width: 50%;
+  text-align: center;
+  align-items: center;
   @media (min-width: 600px) {
-    width: 33%;
-    align-items: center;
-    justify-content: center;
+    display: block;
+    width: fit-content;
+    text-align: right;
   }
 `
 
-const ResetButton = styled.button`
-  background-color: transparent;
-  padding: 0;
-  margin: 0;
-  width: fit-content;
-  font-size: 0.75rem;
-  :hover {
-    cursor: button;
-  }
-`
-
-// TODO: figure out where to put correct answers info
 // TODO: refactor streaks/milestones functionality to minimize duplicate code where possible
 // TODO: make reset button available only during development (or hide it in options menu, eventually)
 
@@ -109,19 +88,14 @@ const Scoreboard = () => {
 
   return (
     <ScoreboardContainer>
-      <StreakContainer>
+      <MobileFlexContainer>
+        <ProgressBar isMilestoneBar currentValue={birdsIdentified} maxValue={maxValue(birdsIdentified, isNextMilestone, isCurrentMilestoneGoal, justAchievedMilestone)}/>
+        <h4 id='answers-label'>Correct Answers: {birdsIdentified}/{birdsSeen} ({ Math.floor((birdsIdentified/birdsSeen) * 100)}%)</h4>
+      </MobileFlexContainer>
+      <MobileFlexContainer id='streak-progress-container'>
         <ProgressBar currentValue={currentStreak} maxValue={maxValue(currentStreak, isNextStreak, isCurrentStreakGoal, justAchievedStreak)}/>
         <h4>Current Streak: {currentStreak}</h4>
-        {/* <ResetButton onClick={() => { localStorage.clear(); window.location.reload() }}>Reset Score</ResetButton> */}
-      </StreakContainer>
-      <StreakContainer>
-        <ProgressBar isMilestoneBar currentValue={birdsIdentified} maxValue={maxValue(birdsIdentified, isNextMilestone, isCurrentMilestoneGoal, justAchievedMilestone)}/>
-        <h4>Milestone Tracker</h4>
-      </StreakContainer>
-      <StreakContainer id='answers-container'>
-        <h2>{birdsIdentified}/{birdsSeen} ({ Math.floor((birdsIdentified/birdsSeen) * 100)}%)</h2>
-        <h4 id='answers-label'>Correct Answers</h4>
-      </StreakContainer>
+      </MobileFlexContainer>
     </ScoreboardContainer>
   )
 }
