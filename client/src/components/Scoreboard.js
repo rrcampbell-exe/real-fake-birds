0b1e79a114014eea22b20f2081a6ddee2224fdc1
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-26 07:54:54 -0500

    fix: remove milestone tracker

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -2,22 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 import ProgressBar from './ProgressBar'
 import streakValues from '../constants/streak-values'
-import milestoneValues from '../constants/milestone-values'
-import { isNextStreakEval, isNextMilestoneEval, currentStreakGoalEval, currentMilestoneGoalEval } from '../utils/goals-evaluation'
+import { isNextStreakEval, currentStreakGoalEval } from '../utils/goals-evaluation'
 import media from '../constants/media'
 
 const ScoreboardContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   margin: 0 1rem 0 1rem;
   font-size: 0.75rem;
   background-color: rgba(0,0,0,.35);
   padding: 0.5rem;
   border: solid ${({ theme }) => theme.textColor} 1px;
   border-radius: 4px;
+  width: 50%;
   h4 {
-    margin-top: 0.25rem;
-    margin-bottom: 0.5rem;
+    margin-top: 0.5rem;
+    margin-bottom: 0rem;
   }
   a {
     margin-top: 0.25rem;
@@ -38,37 +38,7 @@ const ScoreboardContainer = styled.div`
     padding: 0;
     border: none;
     background-color: rgba(0,0,0,0);
-    #streak-progress-container {
-      width: 100%;
-    }
-  }
-`
-
-const MobileFlexContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 50%;
-  text-align: center;
-  align-items: center;
-  h4 {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-  }
-  @media (min-width: ${media.tabletPlus}) {
-    display: block;
     width: fit-content;
-    text-align: right;
-    h4 {
-      flex-direction: row;
-    }
-  }
-`
-
-const Punctuation = styled.span`
-  display: none;
-  @media (min-width: ${media.tabletPlus}) {
-    display: block;
   }
 `
 
@@ -78,12 +48,8 @@ const Scoreboard = () => {
 
   const isNextStreak = isNextStreakEval(streakValues, currentStreak)
   const isCurrentStreakGoal = currentStreakGoalEval(streakValues, currentStreak)
-
-  const isNextMilestone = isNextMilestoneEval(milestoneValues, birdsIdentified)
-  const isCurrentMilestoneGoal = currentMilestoneGoalEval(milestoneValues, birdsIdentified)
   
   const justAchievedStreak = JSON.parse(localStorage.getItem('birdStreakAchieved'))
-  const justAchievedMilestone = JSON.parse(localStorage.getItem('birdMilestoneAchieved'))
 
   const maxValue = (currentValue, isNextGoal, isCurrentGoal, justAchievedGoal) => {
     if ((currentValue === isCurrentGoal) && justAchievedGoal) {
@@ -94,26 +60,9 @@ const Scoreboard = () => {
 
   return (
     <ScoreboardContainer>
-      <MobileFlexContainer>
-        <ProgressBar isMilestoneBar currentValue={birdsIdentified} maxValue={maxValue(birdsIdentified, isNextMilestone, isCurrentMilestoneGoal, justAchievedMilestone)}/>
-        <h4>
-          Correct Answers
-          <Punctuation>:</Punctuation>
-          <span>
-            {birdsIdentified}/{birdsSeen} ({ Math.floor((birdsIdentified/birdsSeen) * 100)}%)
-          </span>
-        </h4>
-      </MobileFlexContainer>
-      <MobileFlexContainer id='streak-progress-container'>
-        <ProgressBar currentValue={currentStreak} maxValue={maxValue(currentStreak, isNextStreak, isCurrentStreakGoal, justAchievedStreak)}/>
-        <h4>
-          Current Streak: {currentStreak}
-          <Punctuation> |</Punctuation>
-          <span>
-            Best: {bestStreak}
-          </span>
-        </h4>
-      </MobileFlexContainer>
+      <ProgressBar currentValue={currentStreak} maxValue={maxValue(currentStreak, isNextStreak, isCurrentStreakGoal, justAchievedStreak)}/>
+      <h4>Current Streak: {currentStreak} | Best Streak: {bestStreak}</h4>
+      <h4>Correct Answers: {birdsIdentified}/{birdsSeen} ({ Math.floor((birdsIdentified/birdsSeen) * 100)}%)</h4>
     </ScoreboardContainer>
   )
 }
