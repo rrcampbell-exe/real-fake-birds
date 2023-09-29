64818c7cc0c566993a64ad71da3a1512141caed2
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-29 16:11:58 -0500

    feat: introduce badges

--- client/src/pages/Menu/components/Badges.js
+++ client/src/pages/Menu/components/Badges.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { getTheme } from '../../../utils/theme-handler'
+import { AdultMilestone, EggMilestone, FledglingMilestone, HatchlingMilestone, JuvenileMilestone, NestlingMilestone } from '../../../constants/badge-svg'
 
 const Badges = () => {
+  const { textColor } = getTheme()
   return (
-    'this is the badges section'
+    <>
+      <EggMilestone color={textColor} size='2rem' />
+      <HatchlingMilestone color={textColor} size='2rem' />
+      <NestlingMilestone color={textColor} size='2rem' />
+      <FledglingMilestone color={textColor} size='2rem' />
+      <JuvenileMilestone color={textColor} size='2.5rem' />
+      <AdultMilestone color={textColor} size='2rem' />
+    </>
   )
 }
 
