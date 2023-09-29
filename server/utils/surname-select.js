5fe863698fd6809e0c0b5db8b3bb3266e6d26f26
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-29 11:02:27 -0500

    feat: institute timeout

--- server/utils/surname-select.js
+++ server/utils/surname-select.js
@@ -1,11 +1,17 @@
 const axios = require('axios')
 
 const surnameSelect = async () => {
-  const response = await axios.get(`https://randomuser.me/api/?nat=us,dk,fr,gb,de,es,ca,mx`) 
-
-  const surname = response.data.results[0].name.last
-
-  return surname
+  try {
+    const response = await axios.get(`https://randomuser.me/api/?nat=us,dk,fr,gb,de,es,ca,mx`, { timeout: 8000 }) 
+  
+    const surname = response.data.results[0].name.last
+  
+    return surname
+  }
+  catch (err) {
+    console.log('surname call timed out')
+    return { error: 'surname call timed out'}
+  }
 }
 
 module.exports = surnameSelect
