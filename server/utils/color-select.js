779b4190c585041f984aba12c4cb0aa5d1d1dd34
rrcampbell-exe <campbell.ryan.r@gmail.com>
2023-09-29 11:04:42 -0500

    feat: institute timeout

--- server/utils/color-select.js
+++ server/utils/color-select.js
@@ -2,9 +2,15 @@ const axios = require('axios')
 
 const colorSelect = async () => {
   const colorVal = Math.floor(Math.random() * 1000000)
-  const response = await axios.get(`https://www.thecolorapi.com/id?hex=${colorVal}`)
-  
-  return response.data.name.value
+  try {
+    const response = await axios.get(`https://www.thecolorapi.com/id?hex=${colorVal}`, { timeout: 8000 })
+    
+    return response.data.name.value
+  }
+  catch (err) {
+    console.log('color call timed out')
+    return { error: 'color call timed out'}
+  }
 }
 
 module.exports = colorSelect
